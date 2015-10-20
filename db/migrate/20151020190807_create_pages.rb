6cd2df1722519225a414a4c6812624cf07b66467
Roope Salmi <rpsalmi@gmail.com>
2015-10-20 22:22:08 +0300

    Add first model: Page(s)

---
+++ db/migrate/20151020190807_create_pages.rb
@@ -0,0 +1,11 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :text_id
+      t.string :title
+      t.text :content
+      
+      t.timestamps null: false
+    end
+  end
+end
