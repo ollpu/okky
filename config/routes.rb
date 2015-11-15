d7a139b91393892b4f7d2c18d48f6d972766e7cd
Roope Salmi <rpsalmi@gmail.com>
2015-11-15 13:34:48 +0200

    Remove ckeditor from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+  # mount Ckeditor::Engine => '/ckeditor'
   root to: 'index#index'
   
   get '/hallinta/sivut/järjestys' => 'pages#order', as: 'admin_pages_order'
