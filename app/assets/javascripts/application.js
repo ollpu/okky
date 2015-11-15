65f98b58c6bca79ee8cf75576186353db003643f
Roope Salmi <rpsalmi@gmail.com>
2015-11-15 11:27:41 +0200

    Temporarily disable ckeditor
    It is causing Javascript to load improperly.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require ckeditor/init
+// require ckeditor/init // Temporarily disable ckeditor
 
 //= require jquery-ui/sortable
